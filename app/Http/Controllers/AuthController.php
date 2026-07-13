e501c825f5a6f6bc9630a33530c65ce345269d8a
Chamroeurn Kimseang <kchamroeurn@paragoniu.edu.kh>
2026-07-13 10:47:34 +0700

    ongoing

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    // Login
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+
+        if (Auth::attempt($credentials, $request->remember)) {
+
+            $request->session()->regenerate();
+
+            return redirect('/dashboard');
+        }
+
+
+        return back()->withErrors([
+            'email' => 'Invalid email or password.'
+        ]);
+    }
+
+
+
+
+    // Register
+public function register(Request $request)
+{
+    $validated = $request->validate([
+
+        'first_name' => 'required|string|max:255',
+
+        'last_name' => 'required|string|max:255',
+
+        'email' => 'required|email|unique:users,email',
+
+        'phone' => 'required|string|max:20',
+
+        'password' => 'required|min:8|confirmed',
+
+    ]);
+
+
+    $user = User::create([
+
+        'first_name' => $validated['first_name'],
+
+        'last_name' => $validated['last_name'],
+
+        'email' => $validated['email'],
+
+        'phone_number' => $validated['phone'],
+
+        'password' => Hash::make($validated['password']),
+
+    ]);
+
+
+    Auth::login($user);
+
+
+    $request->session()->regenerate();
+
+
+    return redirect('/login');
+}
+
+
+
+    // Logout
+    public function logout(Request $request)
+    {
+
+        Auth::logout();
+
+
+        $request->session()->invalidate();
+
+
+        $request->session()->regenerateToken();
+
+
+        return redirect('/login');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
+
+// Home
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Register
+Route::get('/register', function () {
+    return view('admin.auth.register');
+})->name('register');
+
+Route::post('/register', [AuthController::class, 'register']);
+
+
+// Login
+Route::get('/login', function () {
+    return view('admin.auth.login');
+})->name('login');
+
+Route::post('/login', [AuthController::class, 'login']);
+
+
+// // Dashboard
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware('auth');
+
+
+// Logout
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
